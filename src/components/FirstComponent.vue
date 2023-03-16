cf562933b4c573ca1abd0ffe19ac378c828f4d47
João Marcelo Dantas <jmgrd98@gmail.com>
2023-03-16 12:38:10 -0300

    commit

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1>Hello, Vue!</h1>
+  <h1>Hello, my name is {{ nome }}!</h1>
+  <h2>I'm currently working as a {{ role }}</h2>
 </template>
 
 <script>
   export default {
-    name: 'FirstComponent'
+    name: 'FirstComponent',
+    data(){
+      return {
+        nome: 'John Doe',
+        role: "Developer"
+      }
+    }
   }
 </script>
