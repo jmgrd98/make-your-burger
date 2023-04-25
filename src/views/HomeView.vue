a8ff8b4c990325c147d6d80b1bb7413aa78f71b7
João Marcelo Dantas <jmgrd98@gmail.com>
2023-04-25 12:38:01 -0300

    banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,20 @@
 <template>
+  <Banner/>
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Monte o seu burger:</h1>
+    <p>Formulário</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Banner from '@/components/Banner.vue';
 
 export default {
+
   name: 'HomeView',
   components: {
-
-  }
+    Banner
+  },
 }
 </script>
