e437fb73f6ee698ab71e41870fe27d92042e7ce3
João Marcelo Dantas <jmgrd98@gmail.com>
2023-04-25 12:15:36 -0300

    navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav>
+  <nav id="nav">
     <router-link to="/"><img :src="logo" :alt="alt" id="logo"/></router-link>
     <router-link to="/">Home</router-link> |
     <router-link to="/pedidos">Pedidos</router-link>
@@ -15,4 +15,30 @@
 
 <style scoped>
 
+  #nav{
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  #nav a{
+    color: #fcba03;
+    text-decoration: none;
+    margin: 0 10px;
+    transition: .5s;
+  }
+
+  #nav a:hover{
+    color: #fff;
+  }
+
+  #logo{
+    width: 50px;
+    height: 50px;
+    margin-right: auto;
+    justify-self: flex-start;
+  }
 </style>
