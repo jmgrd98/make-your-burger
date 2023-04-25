98053d8ca7508b2516d5e458cdae2ee9dcc1055c
João Marcelo Dantas <jmgrd98@gmail.com>
2023-04-25 15:26:32 -0300

    Update Dashboard.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,15 +11,14 @@
         <div>Ações:</div>
       </div>
       <div id="burger-table-rows">
-        <div class="burger-table-row">
-          <div class="order-number">1</div>
-          <div>João</div>
-          <div>Pão de alho</div>
-          <div>Picanha</div>
+        <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+          <div class="order-number">{{burger.id}}</div>
+          <div>{{ burger.nome }}</div>
+          <div>{{burger.pao}}</div>
+          <div>{{ burger.carne }}</div>
           <div>
             <ul>
-              <li>Salame</li>
-              <li>Tomate</li>
+              <li v-for="(opcional, index) in burger.opcionais" :key="index">{{opcional}}</li>
             </ul>
           </div>
 <!--          <div>Mensagem</div>-->
@@ -38,6 +37,27 @@
 <script>
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: []
+    }
+  },
+  methods: {
+    async getPedidos() {
+      const req = await fetch('http://localhost:3000/burgers');
+      const data = await req.json();
+      this.burgers = data;
+
+      this.burgers.forEach(burger => {
+        this.status.push(burger.status);
+      });
+    },
+  },
+  mounted() {
+    this.getPedidos();
+  }
 }
 </script>
 
@@ -80,14 +100,13 @@ export default {
 
 #status-container{
   display: flex;
-  flex-direction: column;
   gap: 5px;
 }
 
 select {
   padding: 12px 6px;
   margin-right: 12px;
-  max-width: 100%;
+  max-width: 50%;
 }
 
 .delete-btn{
