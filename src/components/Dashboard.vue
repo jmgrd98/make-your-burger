fd15d8b97178a20664993788d9c6203f82124155
João Marcelo Dantas <jmgrd98@gmail.com>
2023-04-29 15:39:14 -0300

    moving data from json-server to actual vue so i can deploy

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -91,6 +91,97 @@ export default {
   },
   mounted() {
     this.getPedidos();
+  },
+  api: {
+    "ingredientes": {
+      "paes": [
+        {
+          "id": 1,
+          "tipo": "Italiano Branco"
+        },
+        {
+          "id": 2,
+          "tipo": "3 Queijos"
+        },
+        {
+          "id": 3,
+          "tipo": "Parmesão e Orégano"
+        },
+        {
+          "id": 4,
+          "tipo": "Integral"
+        }
+      ],
+      "carnes": [
+        {
+          "id": 1,
+          "tipo": "Maminha"
+        },
+        {
+          "id": 2,
+          "tipo": "Alcatra"
+        },
+        {
+          "id": 3,
+          "tipo": "Picanha"
+        },
+        {
+          "id": 4,
+          "tipo": "Veggie burger"
+        }
+      ],
+      "opcionais": [
+        {
+          "id": 1,
+          "tipo": "Bacon"
+        },
+        {
+          "id": 2,
+          "tipo": "Cheddar"
+        },
+        {
+          "id": 3,
+          "tipo": "Salame"
+        },
+        {
+          "id": 4,
+          "tipo": "Tomate"
+        },
+        {
+          "id": 4,
+          "tipo": "Cebola roxa"
+        },
+        {
+          "id": 4,
+          "tipo": "Pepino"
+        }
+      ]
+    },
+    "status": [
+      {
+        "id": 1,
+        "tipo": "Solicitado"
+      },
+      {
+        "id": 2,
+        "tipo": "Em produção"
+      },
+      {
+        "id": 3,
+        "tipo": "Finalizado"
+      }
+    ],
+    "burgers": [
+      {
+        "nome": "Joao Marcelo Guerra Ribeiro Dantas",
+        "pao": "3 Queijos",
+        "carne": "Alcatra",
+        "opcionais": [],
+        "status": "Em produção",
+        "message": "",
+        "id": 1
+      }
+    ]
   }
 }
 </script>
